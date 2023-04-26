a5247c796be9fec902794b83063db16ad032c552
Marcelo Felipe <marcelopdr@yahoo.com.br>
2023-04-26 10:42:18 -0300

    alterado itens lista

--- listaonline/view/tbitens/itens.js
+++ listaonline/view/tbitens/itens.js
@@ -1,10 +1,9 @@
 const xhr = new XMLHttpRequest();
-const urlItem = "http://localhost/listaonline/src/controll/routes/route.item.php";
-var item = document.querySelector("#item");
-var form = document.querySelector('#div');
+const urlItem =
+  "http://localhost/listaonline/src/controll/routes/route.item.php";
+var item = document.querySelector(".item");
+var form = document.querySelector("#div");
 let idse = "";
-var editAlt = true;
-var editAdd = true;
 var listaItens = [];
 var indice = 0;
 
@@ -18,34 +17,31 @@ function readItem() {
     })
     .then(function (data) {
       data.forEach((dado) => {
-        let row = document.createElement("tr");
+        let row = document.createElement("div");
+        row.classList.add("container-item");
+        console.log(row.innerHTML);
         listaItens.push(dado);
-        row.innerHTML += `<td class="item--tabela"><input type="checkbox" onclick="checado(this,${indice})" unchecked class="item--tabela"></td>`;
-        row.innerHTML += `<td class="item--tabela">${dado.nome_item}</td>`;
+        row.innerHTML += `<p class="linha-item">${dado.nome_item}</p>`;
         if (dado.qtd == null) {
-          row.innerHTML += `<td class="item--tabela"></td>`;
+          row.innerHTML += `<p class="linha-item"></p>`;
         } else {
-          row.innerHTML += `<td class="item--tabela">${dado.qtd}</td>`;
+          row.innerHTML += `<p class="linha-item">${dado.qtd}</p>`;
         }
-        row.innerHTML += `<td class="item--tabela"><div class="opcoes--tabela">
-                  <span class='edi' onclick='editItem(this.parentNode.parentNode.parentNode.cells)'>
-                  <i class="fa fa-pencil" aria-hidden="true"></i>
-                  </span>
-                  <span class='del' onclick='delItem(${indice})'>
-                  <i class="fa fa-trash-o" aria-hidden="true"></i>
-                  </span>
-                  <span class='sal' onclick='salvarAlteracao(${indice},this.parentNode.parentNode.parentNode.cells)' hidden=true>
-                  <i class="fa fa-floppy-o" aria-hidden="true"></i>
-                  </span>
-                  <span class='can' onclick='editItem(this.parentNode.parentNode.parentNode.cells)' hidden=true>
-                  <i class="fa fa-times" aria-hidden="true"></i>
-                  </span></div>
-                  </td>`;
+        row.innerHTML += `<div class="item-acoes">
+        <i class="fa fa-pencil" aria-hidden="true" onclick='editItem(this.parentNode.parentNode,${indice})'></i>
+        <i class="fa fa-trash-o" aria-hidden="true" onclick='delItem(${indice})'></i>
+        <input type="checkbox" onclick="checado(this,${indice})" unchecked class="checkbox-acoes">
+        </div>`;
         item.appendChild(row);
         if (dado.concluido == 1) {
-          row.cells[0].children[0].checked = true;
-          row.cells[1].style.textDecoration = 'line-through';
-          row.cells[2].style.textDecoration = 'line-through';
+          row.style.border = "1px solid var(--cor-de-fundo-menu)";
+          row.style.background = "lightblue";
+          row.children[2].children[2].checked = true;
+          row.children[0].style.textDecoration = "line-through";
+          row.children[1].style.textDecoration = "line-through";
+          row.children[0].style.color = "var(--cor-de-fundo-menu)";
+          row.children[1].style.color = "var(--cor-de-fundo-menu)";
+          row.children[2].children[0].style = "Display: none";
         }
         indice++;
       });
@@ -55,16 +51,15 @@ function readItem() {
     });
 }
 
-function editItem(itemEditado) {
-  console.log(itemEditado);
-  itemEditado[1].contentEditable = editAlt;
-  itemEditado[2].contentEditable = editAlt;
-  itemEditado[1].focus();
-  itemEditado[3].children[0].children[0].hidden = editAlt;
-  itemEditado[3].children[0].children[1].hidden = editAlt;
-  itemEditado[3].children[0].children[2].hidden = !editAlt;
-  itemEditado[3].children[0].children[3].hidden = !editAlt;
-  editAlt = !editAlt;
+function editItem(itemEditado,indice) {
+  let table = document.querySelector(".item");
+  table.style.display = "none";
+  let form = document.createElement("form");
+  form.className = "Formulario__item";
+  form.innerHTML += `<h2 class="Titulo__Principal">Alterar Item</h2><input type="text" id="input_item" value="${itemEditado.children[0].innerHTML}">`;
+  form.innerHTML += `<h2 class="Titulo__Principal">Quantidade</h2><input type="text" id="input_qtd" value="${itemEditado.children[1].innerHTML}">`;
+  form.innerHTML += `<div class="container-botao"><input type="button" onclick="cancelarAdicionar()" value="Voltar" class="Botao-form"/><input type="button" onclick="salvarAlteracao(${indice})" value="Alterar Item" class="Botao-form"/></div></form>`;
+  div.appendChild(form);
 }
 
 function delItem(indice) {
@@ -82,7 +77,6 @@ function delItem(indice) {
         if (resposta.hasOwnProperty("erro")) {
           alert(resposta.erro);
         } else {
-          //lista.innerHTML = "";
           alert("Item excluido com sucesso!");
         }
       }
@@ -95,9 +89,9 @@ function delItem(indice) {
   }
 }
 
-function salvarAlteracao(indice, celulas) {
-  listaItens[indice].nome_item = celulas[1].innerText;
-  listaItens[indice].qtd = celulas[2].innerText;
+function salvarAlteracao(indice) {
+  listaItens[indice].nome_item = document.querySelector("#input_item").value;
+  listaItens[indice].qtd = document.querySelector("#input_qtd").value;
   let dados =
     "id_lista=" +
     listaItens[indice].id_lista +
@@ -119,19 +113,20 @@ function salvarAlteracao(indice, celulas) {
         if (resposta.hasOwnProperty("erro")) {
           alert(resposta.erro);
         } else {
-
           alert("Lista alterada com sucesso!");
         }
       }
     });
     xhr.open("PUT", urlItem);
     xhr.send(dados);
-    setTimeout(() => { window.location.reload(); }, 1000);
+    setTimeout(() => {
+      window.location.reload();
+    }, 1000);
   }
 }
 
 function novoItem() {
-  let table = document.querySelector("#table");
+  let table = document.querySelector(".item");
   table.style.display = "none";
   let form = document.createElement("form");
   form.className = "Formulario__item";
@@ -141,21 +136,25 @@ function novoItem() {
   div.appendChild(form);
 }
 
-function cancelarAdicionar(){
-  let table = document.querySelector("#table");
-  table.style.display = "table";
-  form.innerHTML = '';
+
+function cancelarAdicionar() {
+  let table = document.querySelector(".item");
+  table.style.display = "flex";
+  form.innerHTML = "";
 }
 
 function salvarNovoItem() {
-  let novoItem = document.querySelector('#input_item').value;
-  let qtd = document.querySelector('#input_qtd').value;
+  let novoItem = document.querySelector("#input_item").value;
+  let qtd = document.querySelector("#input_qtd").value;
   if (novoItem === "" || qtd === "") {
-    alert("Preencha os Campos Corretamente!")
+    alert("Preencha os Campos Corretamente!");
   } else {
     var dados = new FormData();
     dados.append("id_item", null);
-    dados.append("id_lista", new URL(window.location.href).searchParams.get("id_lista"));
+    dados.append(
+      "id_lista",
+      new URL(window.location.href).searchParams.get("id_lista")
+    );
     dados.append("id_usuario", localStorage.getItem("id_usu"));
     dados.append("nome_item", novoItem);
     dados.append("qtd", qtd);
@@ -175,23 +174,27 @@ function salvarNovoItem() {
       });
       xhr.open("POST", urlItem);
       xhr.send(dados);
-      setTimeout(() => { window.location.reload(); }, 1000);
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
     }
   }
 }
 
 function checado(check, indice) {
-  let nome_item = check.parentNode.parentNode.cells[1];
-  let qtd = check.parentNode.parentNode.cells[2];
+  let nome_item = check.parentNode.parentNode.children[0];
+  let qtd = check.parentNode.parentNode.children[1];
   let nome = localStorage.getItem("nome_usu");
-
+  
   if (check.checked) {
-    nome_item.style.textDecoration = 'line-through';
-    qtd.style.textDecoration = 'line-through';
+    nome_item.style.textDecoration = "line-through";
+    nome_item.style.color = "var(--cor-de-fundo-menu)";
+    qtd.style.textDecoration = "line-through";
+    qtd.style.color = "var(--cor-de-fundo-menu)";
     listaItens[indice].concluido = 1;
   } else {
-    nome_item.style.textDecoration = 'none';
-    qtd.style.textDecoration = 'none';
+    nome_item.style.textDecoration = "none";
+    qtd.style.textDecoration = "none";
     listaItens[indice].concluido = 0;
   }
   let dados =
@@ -214,13 +217,19 @@ function checado(check, indice) {
       if (resposta.hasOwnProperty("erro")) {
         alert(resposta.erro);
       } else {
-        alert(listaItens[indice].concluido == 1 ? "Parabens! Menos uma Pendência, " + nome : "Poxa, " + nome + " Denovo?");
+        alert(
+          listaItens[indice].concluido == 1
+            ? "Parabens! Menos uma Pendência, " + nome
+            : "Poxa, " + nome + " Denovo?"
+        );
       }
     }
   });
   xhr.open("PUT", urlItem);
   xhr.send(dados);
-  setTimeout(() => { window.location.reload(); }, 2000);
+  setTimeout(() => {
+    window.location.reload();
+  }, 2000);
 }
 
 function limpaLocalStorage() {
